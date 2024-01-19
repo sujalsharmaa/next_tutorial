4760165a79385c0369ebb7414665d776139af751
sujalsharmaa <sujalsharma151@gmail.com>
2024-01-19 20:07:03 +0530

    inirial

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: NextRequest){
             id: user._id,
             username: user.username,
             email: user.email
-        }
+        } 
         const token = await jwt.sign(tokenData,process.
             env.TOKEN_SECRET,{expiresIn: "1d"})
 
